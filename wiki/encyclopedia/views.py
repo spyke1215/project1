e98ac749bb3201301aa594cb474d1fe0ae32b59c
dennis dela torre <insyde.dedsec@gmail.com>
2020-07-28 19:44:20 +0800

    search on anywhere is working

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -7,25 +7,31 @@ from markdown2 import Markdown
 from . import util
 markdowner = Markdown()
 
+def search(request):
+
+    search = request.POST.get("search")
+    entry = util.get_entry(search)
+
+    if not entry:
+        return render(request, "encyclopedia/search.html", {
+            "entries": util.list_entries()
+        })
+    else:
+        return redirect(f'/wiki/{search}')
+
 def index(request):
 
     if request.method == "POST":
-
-            search = request.POST.get("search")
-            entry = util.get_entry(search)
-
-            if not entry:
-                return render(request, "encyclopedia/search.html", {
-                    "entries": util.list_entries()
-                })
-            else:
-                return redirect(f'/wiki/{search}')
+        return search(request)
 
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, name):
+
+    if request.method == "POST":
+        return search(request)
 
     entry = util.get_entry(name)
 
