ebb9370c9a70709268c48e552602447c45fbf5c4
dennis dela torre <insyde.dedsec@gmail.com>
2020-07-29 20:11:50 +0800

    bug title is always false, always saying error

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from markdown2 import Markdown
+import os.path
+
+import re
 
 from . import util
 
@@ -26,7 +29,30 @@ def entry(request, name):
 def create(request):
 
     if request.method == "POST":
-        return
+        title = request.POST.get == "title"
+        markdown = request.POST.get == "markdown"
+
+        for entry in util.list_entries():
+
+            if re.search(f"^{title}$",entry,re.IGNORECASE):
+                return render(request, "encyclopedia/entry.html", {
+                    "entry": "Error: Entry already exists",
+                    "name": "Error"
+                }) 
+
+        save_path = "entries"
+
+        complete_title = os.path.join(save_path, f"{title}.md")  
+
+        new_entry = open(complete_title, "w")
+        toFile = raw_input(markdown)
+        new_entry.write(toFile)
+        new_entry.close()
+
+        return render(request, "encyclopedia/entry.html", {
+            "entry": title,
+            "name": title
+        })
 
     else:
         return render(request, "encyclopedia/create.html")
