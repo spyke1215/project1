59d1a4cb6a361a6b6518ca42546c98ad9f773fe7
dennis dela torre <insyde.dedsec@gmail.com>
2020-07-29 22:58:00 +0800

    added comments in views.py

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -10,104 +10,114 @@ from . import util
 
 def search(request):
 
-    search = request.POST.get("search")
-    test = util.get_entry(search)
+    search = request.POST.get("search") # getting data on POST
+    test = util.get_entry(search) # checking if the is a entry already
     
+    # if theres no entry
     if not test:
 
         final = None
 
+        # checking all the entries
         for entry in util.list_entries():
-
+            
+            # checking if the serach has a substring on list of entries
             result = re.search(rf"^{search}\w+|^{search}$",entry,re.IGNORECASE)
 
-            if not result:
+            if not result: # if result is None
                 continue
-            elif re.search(f"^{search}$",entry,re.IGNORECASE):
+            elif re.search(f"^{search}$",entry,re.IGNORECASE): #if entry case insensitive found
                 return redirect(f'/wiki/{result.group()}')
-            else:
+            else: #store the result in final
                 final = result.group()
         
+        #after the loop redirect to search.html
         return render(request, "encyclopedia/search.html", {
             "entries": final
         })
 
+    # if there is entry, redirect to the page
     else:
         return redirect(f'/wiki/{search}')
 
 def index(request):
-
+    
+    # if POST go to search function
     if request.method == "POST":
         return search(request)
 
+    # if GET render index.html
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, name):
 
+    # if post go to search function
     if request.method == "POST":
         return search(request)
 
-    ent = util.get_entry(name)
+    # checking if entry is in the list of entries
+    entry = util.get_entry(name)
 
-    if not ent:
+    if not entry: #if entry is None 
 
-        return render(request, "encyclopedia/entry.html", {
+        return render(request, "encyclopedia/entry.html", { # return an error
             "error": "ERROR 404: Requested page was not found!",
             "name": name
         })
 
-    else:
-        return render(request, "encyclopedia/entry.html", {
-            "entry": ent,
+    else: # if entry has a value
+        return render(request, "encyclopedia/entry.html", { # show the entry to user
+            "entry": entry, 
             "name": name
         })
 
-def edit (request):
+def edit(request):
 
+    # if POST go to create function
     if request.method == "POST":
 
-        return render(request, "encyclopedia/entry.html", {
-            "entry": util.get_entry(ent),
-            "name": test
-        })
+        return create(request)
 
+    # if GET open edit.html
     else:
         
-        entry = open(f"entries/HTML.md", "r")
+        entry = open(f"entries/HTML.md", "r") # dosen't work
         markdown = entry.read()
 
-        return render(request, "encyclopedia/edit.html", {
+        return render(request, "encyclopedia/edit.html", { # render the data to edit
             "title": "HTML",
             "markdown": markdown
         })
 
 def create(request):
 
+    # if POST 
     if request.method == "POST":
-        title = request.POST.get("title")
-        markdown = request.POST.get("markdown")
-        save_path = "entries"
 
-        for entry in util.list_entries():
+        title = request.POST.get("title") # getting title entered by the user
+        markdown = request.POST.get("markdown") # getting markdown entered by the user
 
-            if re.search(f"^{title}$",entry,re.IGNORECASE):
-                return render(request, "encyclopedia/entry.html", {
+        save_path = "entries" # saving the path for entries
+
+        # loop entries
+        for entry in util.list_entries(): 
+
+            if re.search(f"^{title}$",entry,re.IGNORECASE): # if title has already exist
+                return render(request, "encyclopedia/entry.html", { # return an error
                     "error": "Error: Entry already exists",
                     "name": title
                 }) 
 
-        complete_title = os.path.join(save_path, f"{title}.md")  
+        complete_title = os.path.join(save_path, f"{title}.md") # combine the path and the title
 
-        entry = open(complete_title, "w")
-        entry.write(markdown)
-        entry.close()
+        entry = open(complete_title, "w") # open the file
+        entry.write(markdown) # write in the file
+        entry.close() # close file
 
-        return render(request, "encyclopedia/entry.html", {
-            "entry": util.get_entry(title),
-            "name": title
-        })
+        return redirect(f'/wiki/{title}') # redirect to entry to show the user
 
+    # if GET render create.html
     else:
         return render(request, "encyclopedia/create.html")
